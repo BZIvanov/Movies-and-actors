54ce938f96759be639135ad593306daed293bd34
BZIvanov <biser.z.ivanov@gmail.com>
2021-02-12 14:22:31 +0200

    removed old angular 8, kinvey based client

--- src/app/core/interfaces/movie.ts
+++
@@ -1,7 +0,0 @@
-export interface Movie {
-    title: string;
-    year: number;
-    imageUrl: string;
-    description: string;
-    _acl?: Object
-}
--- src/app/shared/constants.ts
+++
@@ -1,5 +0,0 @@
-export default {
-    baseUrl: "https://baas.kinvey.com/",
-    kinveyAppKey: "kid_S1zFMKjWS",
-    kinveyAppSecret: "a9e83d78deba4c2e91e2857f9a249b3f"
-}
